3ac7f6bac19dd9740724761f91d0fd8895469499
UJJAWAL MISHRA <ujjawalmishra202428@gmail.com>
2026-08-16 18:22:18 +0530

    Time: 2 ms (20.85%), Space: 14.3 MB (51.57%) - LeetHub

---
+++ 0948-bag-of-tokens/0948-bag-of-tokens.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int bagOfTokensScore(vector<int>& tokens, int power) {
+        sort(tokens.begin(), tokens.end());
+        int left = 0;
+        int right = tokens.size() - 1;
+        int score = 0;
+        int ans = 0;
+        while (left <= right) {
+            if (power >= tokens[left]) {
+                power -= tokens[left];
+                score++;
+                left++;
+                ans = max(ans, score);
+            }
+            else if (score > 0) {
+                power += tokens[right];
+                score--;
+
+                right--;
+            }
+            else {
+                break;
+            }
+        }
+        return ans;
+    }
+};
