5330942e0223dba363378d5d65b2e0e874fd56c1
UJJAWAL MISHRA <ujjawalmishra202428@gmail.com>
2026-08-18 17:52:26 +0530

    Time: 3 ms (2.5%), Space: 15.1 MB (49.81%) - LeetHub

---
+++ 0330-patching-array/0330-patching-array.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int minPatches(vector<int>& nums, int n) {
+        long long miss=1;
+        int patches=0;
+        int i=0;
+        while (miss<=n){
+            if (i<nums.size() && nums[i]<=miss) {
+                miss+=nums[i];
+                i++;
+            } 
+            else{
+                // as we can take values from the 1 to miss and add in miss getting till the 2*miss
+                miss+=miss;
+                patches++;
+            }
+        }
+        return patches;
+    }
+};
