87dbdfb5f96ceda495d273550665ccebca7c9419
UJJAWAL MISHRA <ujjawalmishra202428@gmail.com>
2026-08-22 17:07:00 +0530

    Time: 3 ms (88.97%), Space: 23.9 MB (31.67%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        sort(intervals.begin(),intervals.end());
+        vector<vector<int>> ans;
+        int start=intervals[0][0];
+        int end=intervals[0][1];
+        for (int i=1;i<intervals.size();i++){
+            int s=intervals[i][0];
+            int e=intervals[i][1];
+            if (s<=end){
+                end=max(end,e);
+            }
+            else{
+                ans.push_back({start, end});
+                start=s;
+                end=e;
+            }
+        }
+        ans.push_back({start, end});
+        return ans;
+    }
+};
