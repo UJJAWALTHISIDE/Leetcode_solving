b6f59ec1c7502982b1505597e15f0cdc86963ab5
UJJAWAL MISHRA <ujjawalmishra202428@gmail.com>
2026-08-24 16:04:58 +0530

    Time: 0 ms (100%), Space: 8.3 MB (61.92%) - LeetHub

---
+++ 3216-lexicographically-smallest-string-after-a-swap/3216-lexicographically-smallest-string-after-a-swap.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    string getSmallestString(string s) {
+        for (int i=0;i<s.size()-1;i++) {
+            int a=s[i]-'0';
+            int b=s[i+1]-'0';
+            if (a%2==b%2 && a>b){
+                swap(s[i],s[i+1]);
+                break;
+            }
+        }
+        return s;
+    }
+};
